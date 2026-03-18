4115f70d98ab123970a52bdca9da272ba9fb2df8
takahito ito <takayan0908@gmail.com>
2026-03-18 17:02:45 +0900

    feat: add src/data/weapons.js

---
+++ src/data/weapons.js
@@ -0,0 +1,41 @@
+/**
+ * 武器定義データ
+ * 各武器の基本パラメータと特殊技の実装を含む
+ */
+export const WEAPON_DEFS = {
+  steam_pipe: {
+    name: 'スチームパイプ',
+    damage: 18,
+    range: 120,
+    maxDurability: 4,
+    special: (scene, owner) => {
+      // 前方扇形に蒸気噴射
+      scene.steamBlast(owner, 180, 250, 12);
+    },
+    bossWeakness: false,
+  },
+  gear_star: {
+    name: '歯車手裏剣',
+    damage: 12,
+    range: 400,
+    maxDurability: 5,
+    special: (scene, owner) => {
+      // 5連射
+      for (let i = 0; i < 5; i++) {
+        scene.time.delayedCall(i * 100, () => scene.throwProjectile(owner, 12));
+      }
+    },
+    bossWeakness: true,
+  },
+  spark_lantern: {
+    name: '電気ランタン',
+    damage: 8,
+    range: 60,
+    maxDurability: 3,
+    special: (scene, owner) => {
+      // 360°放電
+      scene.electricBlast(owner, 120, 20);
+    },
+    bossWeakness: false,
+  },
+};
