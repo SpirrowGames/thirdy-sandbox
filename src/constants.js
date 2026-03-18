efd362994fa86ffedb891f5955ceac46913969f4
takahito ito <takayan0908@gmail.com>
2026-03-18 17:17:32 +0900

    feat: add src/constants.js

---
+++ src/constants.js
@@ -0,0 +1,6 @@
+export const GAME_WIDTH = 960;
+export const GAME_HEIGHT = 540;
+export const GROUND_Y_MIN = 360;
+export const GROUND_Y_MAX = 480;
+export const DEPTH_THRESHOLD = 40;
+export const COMMAND_WINDOW = 400;
