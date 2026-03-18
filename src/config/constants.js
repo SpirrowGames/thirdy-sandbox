29ee3be207871a0763de43e4cf7eaeb2e0d5bcc6
takahito ito <takayan0908@gmail.com>
2026-03-18 17:15:47 +0900

    feat: add src/config/constants.js

---
+++ src/config/constants.js
@@ -0,0 +1,22 @@
+/**
+ * ゲーム全体で使用する定数定義
+ */
+
+// 画面サイズ
+export const GAME_WIDTH = 960;
+export const GAME_HEIGHT = 540;
+
+// 奥行き設定
+export const GROUND_Y_MIN = 360;
+export const GROUND_Y_MAX = 480;
+export const DEPTH_THRESHOLD = 40;
+
+// 入力設定
+export const COMMAND_WINDOW = 400; // ms
+
+// ゲームバランス
+export const COMBO_RESET_TIME = 1500; // ms
+export const HITSTOP_DURATION = 80; // ms
+
+// ステージ設定
+export const STAGE_WIDTH = 3000;
